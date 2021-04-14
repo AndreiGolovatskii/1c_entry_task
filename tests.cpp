663b2eb49ccb297a76881fc4c63374f1c7cff83e
Андрей Головацкий <andi@Andrejs-MacBook-Pro.local>
2021-04-14 23:00:20 +0300

    fix

--- tests.cpp
+++ tests.cpp
@@ -33,19 +33,19 @@ TEST(State, testStack) {
 TEST(Solver, Simple) {
     TGameSolver solver;
 
-    TGameState state_simple(std::vector{ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
-                                        ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
-                                        ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
-                                        ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
-                                        {},
-                                        {},
-                                        {},
-                                        {}});
+    TGameState state_simple(std::vector<std::vector<TCard>>{ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
+                                                            ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
+                                                            ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
+                                                            ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
+                                                            {},
+                                                            {},
+                                                            {},
+                                                            {}});
 
     ASSERT_TRUE(solver.Solve(state_simple) != -1);// solution exists
 
 
-    TGameState state_hopeless(std::vector{
+    TGameState state_hopeless(std::vector<std::vector<TCard>>{
             ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
             ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
             ToCards({0, 1, 2, 3, 4, 5, 6, 7, 8}),
@@ -61,15 +61,15 @@ TEST(Solver, Simple) {
 }
 
 TEST(Solver, SevenStackOneEmpty) {
-    TGameState state_hard(std::vector{ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
-                                      ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
-                                      ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
-                                      ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
+    TGameState state_hard(std::vector<std::vector<TCard>>{ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
+                                                          ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
+                                                          ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
+                                                          ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
 
-                                      ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
-                                      ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
-                                      ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
-                                      {}});
+                                                          ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
+                                                          ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
+                                                          ToCards({8, 7, 6, 5, 4, 3, 2, 1, 0}),
+                                                          {}});
 
     ASSERT_TRUE(TGameSolver::Solve(state_hard) != -1);// solution NOT exists
 }
